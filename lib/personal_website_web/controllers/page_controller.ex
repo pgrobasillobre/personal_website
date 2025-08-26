94f5d1235a95fdeb9047396662d0fe7eb20e7b57
Pablo Grobas Illobre <pablogrobasillobre@MacBook-Air-de-Pablo.local>
2025-08-26 10:47:41 +0200

    about: add controller action

--- lib/personal_website_web/controllers/page_controller.ex
+++ lib/personal_website_web/controllers/page_controller.ex
@@ -4,4 +4,11 @@ defmodule PersonalWebsiteWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  # debugpgi
+  # Classic controller for static or simple pages (non-LiveView).
+  # Renders templates like about.html.heex via render/2.
+  # Used for non-interactive routes like "/about".
+  def about(conn, _params), do: render(conn, :about)
+
 end
