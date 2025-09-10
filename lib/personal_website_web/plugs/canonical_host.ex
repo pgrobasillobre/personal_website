1dbebc5327019311fcfec153ae588d1ef3e747d5
Pablo Grobas Illobre <pablogrobasillobre@MacBook-Air-de-Pablo.local>
2025-09-10 09:01:52 +0200

    Use strict https 301 canonical redirect

--- lib/personal_website_web/plugs/canonical_host.ex
+++ lib/personal_website_web/plugs/canonical_host.ex
@@ -1,20 +1,22 @@
 defmodule PersonalWebsiteWeb.Plugs.CanonicalHost do
   import Plug.Conn
-  alias Phoenix.Controller
 
   @canonical "pgrobasillobre.com"
 
   def init(opts), do: opts
 
+  # Already on apex → do nothing
   def call(%Plug.Conn{host: @canonical} = conn, _opts), do: conn
 
+  # Any other host (www, fly.dev, etc.) → 301 to https://apex
   def call(conn, _opts) do
     qs = if conn.query_string in [nil, ""], do: "", else: "?" <> conn.query_string
     location = "https://#{@canonical}#{conn.request_path}#{qs}"
 
     conn
-    |> Plug.Conn.put_resp_header("location", location)
-    |> Plug.Conn.send_resp(301, "redirect")
-    |> Plug.Conn.halt()
+    |> put_resp_header("location", location)
+    |> put_resp_header("x-canonical-redirect", "1")   # debug header
+    |> send_resp(301, "redirect")
+    |> halt()
   end
 end
