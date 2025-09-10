f85c02dff3d3d2ce35888291bc757bd2d6dd4552
Pablo Grobas Illobre <pablogrobasillobre@MacBook-Air-de-Pablo.local>
2025-09-10 09:34:35 +0200

    force light mode rendering

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "../lib/*_web/**/*.*ex",
     "../lib/*_web/**/*.heex"
   ],
+  darkMode: 'class',
   theme: { extend: {} },
   plugins: [require("@tailwindcss/typography")], // remove if you didn’t install it
 }
